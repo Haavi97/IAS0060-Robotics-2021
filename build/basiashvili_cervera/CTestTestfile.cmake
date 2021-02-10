9cecba97456379d40d528e33d68c6b49b0d6b1ca
fortin <fortin@ttu.ee>
2021-02-10 09:57:26 +0200

    .gitignore added. Interpreter set for the script

--- build/basiashvili_cervera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baska/Apps/ROS/catkin_ws/src/basiashvili_cervera
-# Build directory: /home/baska/Apps/ROS/catkin_ws/build/basiashvili_cervera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
