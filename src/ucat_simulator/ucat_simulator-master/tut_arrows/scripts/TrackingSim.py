88d1cabb9f4a300707080d84e3207847a1c855c2
fortin <fortin@ttu.ee>
2021-02-27 18:25:12 +0200

    Color contouring added

--- src/ucat_simulator/ucat_simulator-master/tut_arrows/scripts/TrackingSim.py
+++ src/ucat_simulator/ucat_simulator-master/tut_arrows/scripts/TrackingSim.py
@@ -7,8 +7,11 @@
 """
 
 import rospy
-
+import cv2    # This imports openCV library
 import numpy as np
+
+
+from cv_bridge import CvBridge, CvBridgeError  # This imports CvBridge
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import WrenchStamped, Pose, Vector3Stamped, Twist, Point, Vector3
@@ -62,6 +65,22 @@ class CameraBasedControl:
 
         self.count = 0
 
+        # Task 3 added from Moodle
+        self.camera_sub = rospy.Subscriber(
+            "/ucat0/camera/image", Image, self.onCamera)  # Subscriber to images topic
+        # CvBridge will be used to convert the image into a usable format for openCV
+        self.bridge = CvBridge()
+        self.image = None  # This is where the image will be stored
+        # Variable used to chose if user wants to display the images or not (1 for yes)
+        self.show = 1
+
+        # Object detection attributes:
+        self.mask = None
+        self.red_low = np.array([170, 50, 10])
+        self.red_up = np.array([180, 255, 255])
+
+        self.contours = None
+
     # callback function of imu topic subscriber
 
     def imuCallback(self, imu_msg):
@@ -98,7 +117,7 @@ class CameraBasedControl:
 
         # Forces to apply : U = [surge, sway, heave, roll, pitch, yaw]
         # ===================================================================
-        yawU = 0    
+        yawU = 0
         depthU = Kp * e1[2] + D * e2[2] + I * sum(self.depthErrorQueue)
         surgeU = 0
         self.U = [surgeU, 0, depthU, 0, 0, yawU]
@@ -119,6 +138,69 @@ class CameraBasedControl:
     def depthCallback(self, msg):
         pressure = msg.fluid_pressure
         self.depth = (pressure-101325)/(1000*9.81)
+
+    def onCamera(self, image):
+        try:
+            # converting the image into bgr format
+            cv_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+            self.image = cv_image  # Storing the image
+            self.showImage(self.image)  # Calling function to display the image
+
+            # Object detection:
+            frame_HSV = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+            self.mask = cv2.inRange(frame_HSV, self.red_low, self.red_up)
+
+            self.showMask(self.mask)
+
+            # Contours
+            _, self.contours, _ = cv2.findContours(
+                self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            c = max(self.contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(c)
+            center, radius = cv2.minEnclosingCircle(c)
+            cv2.rectangle(cv_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.drawContours(cv_image, self.contours[1], -1, (0, 255, 0), 3)
+            self.showContours(cv_image)
+        except CvBridgeError as e:
+            print(e)
+
+    def showImage(self, image):
+        # Had to change this line. Probably is better to put a try/except statement
+        # Make sure image is not empty and user want to display the image
+        if(image.any() != None and self.show):
+            # This displays the image in a new window
+            cv2.imshow('U-CAT CAMERA', image)
+        # If user presses "q" on keyboard, stop displaying the images
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.show = 0
+            cv2.destroyAllWindows()  # This closes all openCV windows
+
+    def showMask(self, mask):
+        # Had to change this line. Probably is better to put a try/except statement
+        if self.show:
+            try:
+                # This displays the image in a new window
+                cv2.imshow('U-CAT RED MASK', mask)
+            except:
+                if mask == None:
+                    rospy.loginfo("Mask is None")
+        # If user presses "q" on keyboard, stop displaying the images
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.show = 0
+            cv2.destroyAllWindows()  # This closes all openCV windows
+
+    def showContours(self, image):
+        # Had to change this line. Probably is better to put a try/except statement
+        if self.show:
+            try:
+                cv2.imshow('U-CAT contours', image)
+            except:
+                if image == None:
+                    rospy.loginfo("Mask is None")
+        # If user presses "q" on keyboard, stop displaying the images
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            self.show = 0
+            cv2.destroyAllWindows()  # This closes all openCV windows
 
 
 if __name__ == '__main__':
